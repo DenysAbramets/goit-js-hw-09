e2ddaa7be3dbb56e605ce4674267de646c02e2dc
DenysAbramets <abramets_denys@outlook.com>
2023-03-17 13:27:50 +0100

    test

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.6.min.css';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', submitForm)
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(()=> {
+      if (shouldResolve) {
+        resolve({position, delay})
+      } else {
+        reject({position, delay})
+      }
+    }, delay)
+  })
+  
+};
+
+
+
+
+function submitForm (event){
+  event.preventDefault();
+
+ let firstDelay = Number(formEl.delay.value);
+
+ for(let i = 1; i <= formEl.amount.value; i +=1 ){
+  createPromise(i, firstDelay)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+  firstDelay += Number(formEl.step.value);
+ }
+
+
+
 }
